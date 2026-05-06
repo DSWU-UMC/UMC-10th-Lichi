0962b48d47b986cb12479811ed33d0ac6b14ada7
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-05-07 00:26:02 +0900

    feat: 페이지 버튼, 로딩 스피너

--- week3/src/App.tsx
+++ week3/src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
 import MoviePage from "./pages/MoviePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>홈 페이지입니다.</h1>,
+  },
+  {
+    path: "/movies",
+    element: <MoviePage />,
+  },
+]);
 
 function App(): Element {
   // console.log(import.meta.env.VITE_TMDB_KEY);
-  return <MoviePage />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
